91b775d4e582cd160faad5aa36889b916143ddc6
gpinto <giuseppe.pinto@lastminute.com>
2018-12-15 11:26:18 +0100

    completed tests for standardLine parallelism

--- src/test/java/StandardForm/ParallelismOfStandardFormTest.java
+++ src/test/java/StandardForm/ParallelismOfStandardFormTest.java
@@ -7,6 +7,7 @@ import domain.line.StandardLine;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class ParallelismOfStandardFormTest
@@ -28,11 +29,115 @@ public class ParallelismOfStandardFormTest
     Point c = new Point(2d, 3d);
     Point d = new Point(4d, 6d);
 
-    StandardLine firstLine = new StandardLine(a, b);
-    StandardLine secondLine = new StandardLine(c, d);
-
-    assertTrue(conditionalContext.checkParallelism(firstLine, secondLine));
+    assertTrue(conditionalContext
+      .checkParallelism(new StandardLine(a, b), new StandardLine(c, d)));
   }
 
+  @Test
+  public void notParallelInFirstQuadrant()
+  {
+    Point a = new Point(2d, 3d);
+    Point b = new Point(3d, 6d);
+    Point c = new Point(0d, 3d);
+    Point d = new Point(4d, 5d);
 
+    assertFalse(conditionalContext
+      .checkParallelism(new StandardLine(a, b), new StandardLine(c, d)));
+  }
+
+  @Test
+  public void notParallelInSecondQuadrant()
+  {
+    Point a = new Point(-2d, 3d);
+    Point b = new Point(-44d, 6d);
+    Point c = new Point(-0.2d, 3d);
+    Point d = new Point(-2.4d, 6d);
+
+    assertFalse(conditionalContext
+      .checkParallelism(new StandardLine(a, b), new StandardLine(c, d)));
+  }
+
+  @Test
+  public void parallelismInSecondQuadrant()
+  {
+    Point a = new Point(-2d, 3d);
+    Point b = new Point(-4d, 6d);
+    Point c = new Point(-2.2d, 3d);
+    Point d = new Point(-4.2d, 6d);
+
+    assertTrue(conditionalContext
+      .checkParallelism(new StandardLine(a, b), new StandardLine(c, d)));
+  }
+
+  @Test
+  public void parallelismInThirdQuadrant()
+  {
+    Point a = new Point(-2d, -3d);
+    Point b = new Point(-4d, -6d);
+    Point c = new Point(-2.2d, -3d);
+    Point d = new Point(-4.2d, -6d);
+
+    assertTrue(conditionalContext
+      .checkParallelism(new StandardLine(a, b), new StandardLine(c, d)));
+  }
+
+  @Test
+  public void notParallelInThirdQuadrant()
+  {
+    Point a = new Point(-2d, -3d);
+    Point b = new Point(-0.4d, -6d);
+    Point c = new Point(-2.2d, -3d);
+    Point d = new Point(-4.2d, -6d);
+
+    assertFalse(conditionalContext
+      .checkParallelism(new StandardLine(a, b), new StandardLine(c, d)));
+  }
+
+  @Test
+  public void parallelismInFourthQuadrant()
+  {
+    Point a = new Point(2d, -3d);
+    Point b = new Point(4d, -6d);
+    Point c = new Point(2.2d, -3d);
+    Point d = new Point(4.2d, -6d);
+
+    assertTrue(conditionalContext
+      .checkParallelism(new StandardLine(a, b), new StandardLine(c, d)));
+  }
+
+  @Test
+  public void notParallelInFourthQuadrant()
+  {
+    Point a = new Point(2d, -3d);
+    Point b = new Point(4d, -6d);
+    Point c = new Point(2.2d, -3.4d);
+    Point d = new Point(4.2d, -6.5d);
+
+    assertFalse(conditionalContext
+      .checkParallelism(new StandardLine(a, b), new StandardLine(c, d)));
+  }
+
+  @Test
+  public void parallelismOfTwoLinesOnAbscissa()
+  {
+    Point a = new Point(2d, 0d);
+    Point b = new Point(4d, 0d);
+    Point c = new Point(2.2d, 0d);
+    Point d = new Point(4.2d, 0d);
+
+    assertTrue(conditionalContext
+      .checkParallelism(new StandardLine(a, b), new StandardLine(c, d)));
+  }
+
+  @Test
+  public void parallelismInTwoDifferentQuadrant()
+  {
+    Point a = new Point(2d, 24d);
+    Point b = new Point(4d, 0.3d);
+    Point c = new Point(-2d, -24d);
+    Point d = new Point(-4d, -0.3d);
+
+    assertTrue(conditionalContext
+      .checkParallelism(new StandardLine(a, b), new StandardLine(c, d)));
+  }
 }
